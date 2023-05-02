367747e56e4b0e5e20320ad6f5d5ab23b2385b73
hbashar434 <h.bashar434@gmail.com>
2023-05-02 12:56:47 +0600

    chef route set

--- index.js
+++ index.js
@@ -3,8 +3,14 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
+const chef = require("./data/chef.json");
+
 app.get("/", (req, res) => {
   res.send("chef-recipe is Running!");
+});
+
+app.get("/chef", (req, res) => {
+  res.send(chef);
 });
 
 app.listen(port, () => {
